7e68cc9889226e7b4348197caa89ec5b10109e49
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2024-07-18 17:17:18 -0300

    initializing project

---
+++ workit/workit/src/main/java/com/example/workit/WorkitApplication.java
@@ -0,0 +1,13 @@
+package com.example.workit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkitApplication.class, args);
+	}
+
+}
