3ec606181d4b45c25ba91c61ca6f555a40a8e507
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2024-07-19 13:57:37 -0300

    updating login page styles

--- workit/workit/src/main/resources/static/js/login-script.js
+++ workit/workit/src/main/resources/static/js/login-script.js
@@ -1,27 +1,5 @@
 $(document).ready(function() {
-    const themeToggleCheckbox = $('#theme-toggle-checkbox');
-    themeToggleCheckbox.on('change', function() {
-        if (this.checked) {
-            $('body').removeClass('light-mode').addClass('dark-mode');
-            document.cookie = "theme=dark; path=/";
-        } else {
-            $('body').removeClass('dark-mode').addClass('light-mode');
-            document.cookie = "theme=light; path=/";
-        }
-    });
-
-    const cookies = document.cookie.split(';');
-    cookies.forEach(cookie => {
-        const [name, value] = cookie.split('=').map(c => c.trim());
-        if (name === 'theme' && value === 'dark') {
-            $('body').addClass('dark-mode');
-            themeToggleCheckbox.prop('checked', true);
-        } else {
-            $('body').addClass('light-mode');
-        }
-    });
-
-    $('#login-form').on('submit', function(event) {
+     $('#login-form').on('submit', function(event) {
         event.preventDefault();
 
         const email = $('#email').val();
